2a429d0987145154e296c84289e20ba1b7dffe5a
yuta iwasawa <iwasi_44@hotmail.com>
2021-10-21 09:55:16 +0900

    タスク投稿機能、一覧表示機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,23 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(tasks_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+  def show
+    
+  end
+  private
+  def tasks_params
+    params.require(:task).permit(:task_name, :content)
+  end
 end
