5cda31d420b914feeb819f57fc2c2a3a990e4d6a
yuta iwasawa <iwasi_44@hotmail.com>
2021-10-21 11:08:01 +0900

    タスク編集機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,13 +8,25 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(tasks_params)
     if @task.save
-      redirect_to tasks_path
+      redirect_to tasks_path, notice: 'タスクを登録しました'
     else
       render :new
     end
   end
   def show
     @task = Task.find(params[:id])
+  end
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(tasks_params)
+      redirect_to tasks_path, notice: 'タスクを編集しました'
+    else
+      render :edit
+    end
+    
   end
   private
   def tasks_params
