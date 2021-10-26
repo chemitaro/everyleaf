45f55f2ce69d93de2e2da4866a7b287270c0a280
yuta iwasawa <iwasi_44@hotmail.com>
2021-10-26 10:46:47 +0900

    task_nameにindex

---
+++ db/migrate/20211026014313_add_tasks_index_to_task_name_index.rb
@@ -0,0 +1,5 @@
+class AddTasksIndexToTaskNameIndex < ActiveRecord::Migration[6.0]
+  def change
+    add_index :tasks, :task_name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_003514) do
+ActiveRecord::Schema.define(version: 2021_10_26_014313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_10_25_003514) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deadline", default: -> { "now()" }, null: false
     t.string "status", default: "未着手", null: false
+    t.index ["task_name"], name: "index_tasks_on_task_name"
   end
 
 end
