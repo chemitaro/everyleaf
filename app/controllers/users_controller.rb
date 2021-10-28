21ff3026361f62137528b3d82d1babc849b304e9
yuta iwasawa <iwasi_44@hotmail.com>
2021-10-28 14:42:30 +0900

    vallidates emailに追加、ユーザー登録後にログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = '新規ユーザーを登録しました'
+      redirect_to(tasks_path)
     else
       flash.now[:danger] = '正しい情報を入力してください'
       render :new
