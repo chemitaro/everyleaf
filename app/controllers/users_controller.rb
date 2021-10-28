dd151f098e19542525126751d58a4b1d85fd00f8
yuta iwasawa <iwasi_44@hotmail.com>
2021-10-28 10:24:33 +0900

    ユーザー登録機能実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = '新規ユーザーを登録しました'
+    else
+      flash.now[:notice] = '正しい情報を入力してください'
+      render :new
+    end
+  end
+  def show
+    @user = User.find(params[:id])
+
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
