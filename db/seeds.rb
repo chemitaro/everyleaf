4e0163ae0fa3257b746a443fde271a73f9615bba
yuta iwasawa <iwasi_44@hotmail.com>
2021-10-29 11:21:36 +0900

    管理者作成機能

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create([{name: 'Taro', email: 'taro@email.com', password: 'password', admin: true}, 
+User.create([{name: 'Admin', email: 'admin@email.com', password: 'password', admin: true},
+              {name: 'Taro', email: 'taro@email.com', password: 'password', admin: false},
               {name: 'Jiro', email: 'jiro@email.com', password: 'password', admin: false}])
