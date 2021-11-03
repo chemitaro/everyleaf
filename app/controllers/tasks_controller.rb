6341164e788c66a3cf152b33b3310aa347a7f046
yuta iwasawa <iwasi_44@hotmail.com>
2021-11-03 15:00:44 +0900

    ラベル検索機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,20 @@
 class TasksController < ApplicationController
   def index
     @tasks = current_user.tasks
-    # if params[:select].present?
-    #   @tasks = @tasks.search(params[:select]) if params[:select][:search].present?
-    #   @tasks = @tasks.status(params[:select])
-    #   # @tasks = @tasks.sort_index(params[:select]) if params[:select][:sort_index].present?
-    # end
-    # @tasks = @tasks.search(params[:select]) if params[:select].presence?
-
-    # @tasks = @tasks.search(params[:select]) if params[:select].present?
-
     if params[:select].present?
       @tasks = @tasks.search_status(params[:select][:search], params[:select][:status]) if params[:select][:search].present? && params[:select][:status].present?
       @tasks = @tasks.search(params[:select][:search]) if params[:select][:search].present?
       @tasks = @tasks.status(params[:select][:status]) if params[:select][:status].present?
+      if params[:select][:label_id].present?
+        task_label_selections = @tasks
+        @tasks = @tasks.where(task_name: '')
+        task_ids = Labeling.where(label_id: params[:select][:label_id]).pluck(:task_id)
+        task_ids.each do |task_id|
+          @tasks = @tasks.or(task_label_selections.where(id: task_id))
+        end
+      end
+    
     end
-
     @tasks = @tasks.order(deadline: :desc) if params[:sort_expired]
     @tasks = @tasks.order(priority: :desc) if params[:sort_priority]
     @tasks = @tasks.page(params[:page]).per(5)
